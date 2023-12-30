20e96e91f73f2eb422429b19496cd844bade68ba
Ahmar Hafeez <ehmargondal@gmail.com>
2023-12-30 23:35:02 +0500

    Updated functionality so Conext Can be Mantained for chats

--- routes/query_gpt.py
+++ routes/query_gpt.py
@@ -1,17 +1,44 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from utils.gpt_query import query_gpt
+from typing import List 
 
 
 
 router = APIRouter()
 
 @router.post("/query_gpt/")
-async def query_gpt_route(query: str, bird: str, ):
+async def query_gpt_route( query: str, bird: str, messages: List[dict[str,str]] = None):
+    
+    
     try:
-        # Save the uploaded image temporarily
-        gpt_response = await query_gpt(query=query,bird=bird)
-        # Return the prediction as JSON
-        return JSONResponse(content={"query_response": gpt_response }, status_code=200)
+        gpt_response = await query_gpt(bird=bird,query=query,messages=messages)
+    
+        return JSONResponse(
+                content=
+                {
+                    "Query Response": gpt_response, 
+                
+                }, status_code=200)
+    
+    
     except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+
+
+    # try:
+    #     # Save the uploaded image temporarily
+        
+    #     gpt_response, messages = await query_gpt(query=query,bird=bird,messages=messages)
+        
+    #     # Return the prediction as JSON
+    #     return JSONResponse(
+    #         content=
+    #         {
+    #             "query_response": gpt_response,
+    #             "messages": messages 
+    #             }, 
+    #         status_code=200)
+
--- utils/gpt_query.py
+++ utils/gpt_query.py
@@ -3,35 +3,43 @@ import os
 from typing import List 
 
 
+async def query_gpt(
+    query: str, 
+    bird: str, 
+    messages: List = None
+    ):
+    
 
-async def query_gpt(query: str, bird: str):
-    
-    prompt = f"""
-You are a bird expert who is going to answer any question about the following bird {bird}
-"""
-    
-    
+    prompt = f"You are a bird expert. You have the knowledge regarding all the species of bird and today your are going to answer all the question regarding {bird}"
+
     
     
     api_key = os.getenv("OPENAI_API_KEY")
     client = OpenAI()
     try:
-        messages = [{"role": "system", "content": prompt},
-                        {"role": "user", "content": query}
-              ]
-        completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-        )
-        gpt_response = completion.choices[0].message.content
+        if(messages == None):
+            messages = [
+                            {"role": "system", "content": prompt},
+                            {"role": "user", "content": query},
+                       ]
+        else:
+             temp_messages = [
+                            {"role": "system", "content": prompt}
+                       ]
+             messages = temp_messages + messages
+             messages.append( {"role": "user", "content": query})
 
-        return {
-            "query_response": gpt_response
-            }
+        completion = client.chat.completions.create(
+             model="gpt-3.5-turbo",
+            messages=messages
+        )
+        
+        gpt_response = completion.choices[0].message.content
+        messages.append({"role": "system", "content": gpt_response})
+        return gpt_response
         
     except Exception as e:
         print(e)
-
         return ""
 
     
